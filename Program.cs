25e98c89a11fc140bace249c26555003c95f8487
peternkane <peternkane24@gmail.com>
2021-05-28 01:55:34 +0300

    new changes updated

--- Program.cs
+++ Program.cs
@@ -9,42 +9,42 @@ namespace sess4local
         static void Main(string[] args)
         {
             //Space traveller age in seconds
-            decimal spacetravelorAGE = 631154000M;
+            int spacetravelorAGE = 631154000;
+            // decimal spacetravelorAGE = 631154000f;
 
             //Earth orbital period in seconds
-            decimal earthseconds = 31557600M;
+            int earthseconds = 31557600;
+            // decimal earthseconds = 31557600f;
 
             //Planet names and their orbital periods
             string[] planetnames = new string[4];
-            decimal[] orbitalyears = new decimal[4];
+            float[] orbitalyears = new float[4];
             planetnames[0] = "Venus age :";
-            orbitalyears[0] = 0.61519726M;
-            planetnames[1] = "Earth age :";
-            orbitalyears[1] = 0.00M;
-            planetnames[2] = "Mars age :";
-            orbitalyears[2] = 1.8808158M;
-            planetnames[3] = "Jupiter age :";
-            orbitalyears[3] = 11.86261M;
+            orbitalyears[0] = 0.61519726f;
+            // planetnames[1] = "Earth age :";
+            // orbitalyears[1] = 0.00f;
+            planetnames[1] = "Mars age :";
+            orbitalyears[1] = 1.8808158f;
+            planetnames[2] = "Jupiter age :";
+            orbitalyears[2] = 11.862615f;
+            planetnames[3] = "Neptune age :";
+            orbitalyears[3] = 164.79132f;
 
                        
             //Calculation of space travelor age(in years) on earth
-            decimal ageinyears = spacetravelorAGE/earthseconds;
-            decimal amount1 = Math.Round(ageinyears, 2);
+            float ageinyears = spacetravelorAGE/earthseconds;
+            float amount1 = (float)Math.Round(ageinyears, 2);
             Console.WriteLine("Space traveller is "+ amount1 + " years old");
 
             for(int i = 0;  i < planetnames.Length; i++ ){  
                
-                    foreach(var o in orbitalyears){ 
+                    // foreach(var o in orbitalyears){ 
 
-                        decimal Amount2 = (orbitalyears[i] + amount1);
-                        decimal amount3 = Math.Round(Amount2, 2);
-
-
-                        //  Console.WriteLine(String.Format("{0}"+ planetnames[i] + " " + (orbitalyears[i]+ageinyears) + " Earth-years old"));
-
+                        float Amount2 = (amount1 / orbitalyears[i]);
+                        float amount3 = MathF.Round(Amount2, 2);
                         Console.WriteLine(planetnames[i] + " " + amount3 + " Earth-years old");
-                        break;                                              
-                    }             
+                        // break;                                              
+                    // }             
             } 
 
 
